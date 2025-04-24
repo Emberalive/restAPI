1d591e2000c5f3e3b7c53fc62673a394e0433be2
samuel <samueltsutcliffe@gmail.com>
2025-04-24 15:49:10 +0100

    added a get and post method

--- api.php
+++ api.php
@@ -1,11 +1,14 @@
 <?php
-class db_access {
-    private  $host = "165.227.235.122";
-    private  $user = "ss2979_samuel";
-    private  $pass = "QwErTy1243!";
-    private  $db = "ss2979_restAPI";
-    private  $conn;
-    function __construct() {
+class db_access
+{
+    private $host = "165.227.235.122";
+    private $user = "ss2979_samuel";
+    private $pass = "QwErTy1243!";
+    private $db = "ss2979_restAPI";
+    private $conn;
+
+    function __construct()
+    {
         $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
 
         if ($this->conn->connect_error) {
@@ -14,27 +17,77 @@ class db_access {
             printf("Connected successfully: %s\n", $this->conn->host_info);
         }
     }
-    function __destruct() {
+
+    function __destruct()
+    {
         if ($this->conn) {
             $this->conn->close();
             printf("Connection closed: %s\n", $this->conn->connect_error);
-        } else {
-            print("Connection could not be closed: " . $this->conn->connect_error);
+            if ($this->conn->connect_error) {
+                print("Connection could not be closed: " . $this->conn->connect_error);
+            }
         }
     }
-    function GET($source, $target) {
-        $stmnt = $this->conn->prepare("SELECT * FROM messages WHERE source = ? AND target = ?");
-        $stmnt->bind_param("ss", $source, $target);
 
-        $stmnt->execute();
-        $result = $stmnt->get_result();
+    //this is getting the messages sent from someone else and received by a specific user
+    function GET($source, $target)
+    {
+        $sent_stmnt = $this->conn->prepare("SELECT * FROM messages WHERE source = ? AND target = ?");
+        $sent_stmnt->bind_param("ss", $source, $target);
 
-        for ($i = 0; $i < $result->num_rows; $i++) {
-            $row = $result->fetch_assoc();
+        $sent_stmnt->execute();
+        $sent_result = $sent_stmnt->get_result();
+
+        $recieved_stmnt = $this->conn->prepare("SELECT * FROM messages WHERE source = ? AND target = ?");
+        $recieved_stmnt->bind_param("ss", $target, $source);
+
+        $recieved_stmnt->execute();
+        $recived_result = $recieved_stmnt->get_result();
+
+
+//        $num_rows = $sent_result->num_rows + $recived_result->num_rows;
+        $messages = [];
+
+        while ($row = $recived_result->fetch_assoc()) {
+            array_push($messages, $row);
+        }
+
+        // Fetch sent messages
+        while ($row = $sent_result->fetch_assoc()) {
+            array_push($messages, $row);
+        }
+
+        if (count($messages) > 0) {
+            $json_data = [];
+
+            // Encode each message to JSON
+            foreach ($messages as $message) {
+                // Store the JSON-encoded message
+                $json_data[] = json_encode($message);
+            }
+            file_put_contents("messages.json", implode(PHP_EOL, $json_data));
         }
     }
-}
+        //This is creating a message insertion into the database
+        function POST($source, $target, $message)
+        {
+            $stmnt = $this->conn->prepare("INSERT INTO messages(target, source, message)
+          VALUES(?, ?, ?)");
+            $stmnt->bind_param("sss", $target, $source, $message);
+            $stmnt->execute();
 
-$conn = new db_access()
-$conn->GET()
+            $result = $stmnt->get_result();
+            for ($i = 0; $i < $result->num_rows; $i++) {
+                $row = $result->fetch_assoc();
+            }
+        }
+    }
+
+$conn = new db_access();
+
+
+$conn->POST("name", "name", "This is a message");
+
+$conn->GET("name", "name");
+
 ?>
