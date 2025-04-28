124a7dbbaef28ae01ed3e7c665fa664150e90a83
Sammy Boy <samueltsutcliffe@gmail.com>
2025-04-28 22:35:26 +0100

    I have updated the responses and better accommodated it to the assessment brief

--- api.php
+++ api.php
@@ -1,95 +1,152 @@
 <?php
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
-//
-//header('Content-Type: application/json');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+header('Content-Type: application/json');
+
+//only allowing post and get requests
 //$method = $_SERVER['REQUEST_METHOD'];
 //if (!in_array($method, ['GET', 'POST'])) {
 //    http_response_code(405); // Method Not Allowed
 //    exit;
 //}
-class db_access
-{
+class db_access {
     private $host = "165.227.235.122";
     private $user = "ss2979_samuel";
     private $pass = "QwErTy1243!";
     private $db = "ss2979_restAPI";
     private $conn;
 
-    function __construct()
-    {
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
-
-        if ($this->conn->connect_error) {
-            HTTP_response_code(500);
-        } else {
-            HTTP_response_code(200);
+    //create the connection to a database
+    public function __construct() {
+        try {
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        } catch (mysqli_sql_exception $e) {
+            http_response_code(500);
+            echo json_encode(["error" => $e->getMessage()]);
         }
     }
 
-    function __destruct()
+    public function getConnection() {
+        if ($this->conn) {
+            return $this->conn;
+        }
+        return null;
+    }
+
+    //close the connection to the database
+    public function __destruct()
     {
         if ($this->conn) {
             $this->conn->close();
         }
     }
+}
+class messageService {
+    private $conn;
+    function __construct(db_access $dbConnection){
+        $this->conn = $dbConnection->getConnection();
+    }
 
     //this is getting the messages sent from someone else and received by a specific user
-    function GET($source, $target)
-    {
-        $sent_stmnt = $this->conn->prepare("SELECT * FROM message WHERE source = ? AND target = ?");
-        $sent_stmnt->bind_param("ss", $source, $target);
+    function GET($source, $target) {
 
-        $sent_stmnt->execute();
-        $sent_result = $sent_stmnt->get_result();
+        if (empty($source) || empty($target)) {
+            http_response_code(400);
+            echo json_encode(["Invalid parameters!"]);
+            return false;
+        }
+        try {
+            $sent_stmnt = $this->conn->prepare("SELECT * FROM message WHERE source = ? AND target = ?");
+            $sent_stmnt->bind_param("ss", $source, $target);
 
-        $recieved_stmnt = $this->conn->prepare("SELECT * FROM message WHERE source = ? AND target = ?");
-        $recieved_stmnt->bind_param("ss", $target, $source);
+            $sent_stmnt->execute();
+            $sent_result = $sent_stmnt->get_result();
 
-        $recieved_stmnt->execute();
-        $recived_result = $recieved_stmnt->get_result();
+            $recieved_stmnt = $this->conn->prepare("SELECT * FROM message WHERE source = ? AND target = ?");
+            $recieved_stmnt->bind_param("ss", $target, $source);
 
+            $recieved_stmnt->execute();
+            $recived_result = $recieved_stmnt->get_result();
 
-        $messages = [];
+        } catch (mysqli_sql_exception $e) {
+            http_response_code(500);
+            echo json_encode(["error" => $e->getMessage()]);
+            return false;
+        }
+
+        $messages = [
+            "sent_from" => [],
+            "received_by" => []
+        ];
 
         while ($row = $recived_result->fetch_assoc()) {
-            array_push($messages, $row);
+            array_push($messages['received_by'], $row);
         }
 
         // Fetch sent messages
         while ($row = $sent_result->fetch_assoc()) {
-            array_push($messages, $row);
+            array_push($messages['sent_from'], $row);
         }
 
-        if (count($messages) > 0) {
+        if (!$messages['received_by'] && !$messages['sent_from']) {
+            http_response_code(204);
+            echo json_encode(["Invalid parameters!"]);
+            return false;
+        } else {
             $json_data = json_encode($messages, JSON_PRETTY_PRINT);
 
             file_put_contents("messages.json", $json_data, FILE_APPEND);
             echo $json_data;
+            http_response_code(200);
         }
+        return true;
     }
-        //This is creating a message insertion into the database
-        function POST($source, $target, $message)
-        {
-            $stmnt = $this->conn->prepare("INSERT INTO message(target, source, text)
-          VALUES(?, ?, ?)");
+
+    //This is creating a message insertion into the database
+    function POST($source, $target, $message) {
+        if (empty($source) || empty($target) || empty($message)) {
+            http_response_code(400);
+            echo json_encode(["Invalid parameters!"]);
+            return false;
+        }
+        try{
+            $stmnt = $this->conn->prepare("INSERT INTO message (target, source, text)
+      VALUES(?, ?, ?)");
             $stmnt->bind_param("sss", $target, $source, $message);
             $stmnt->execute();
 
+            if ($stmnt->affected_rows > 0) {
+                $check_stmt  = $this->conn->prepare("SELECT LAST_INSERT_ID();");
+                $check_stmt->execute();
+                $check_results = $check_stmt->get_result();
+                $id = $check_results->fetch_assoc();
+                http_response_code(201);
+                echo json_encode($id);
+                return true;
+            } else {
+                http_response_code(500);
+                return false;
+            }
+        } catch (mysqli_sql_exception $e){
+            HTTP_response_code(500);
+            echo json_encode(["error" => $e->getMessage()]);
+            return false;
         }
     }
+}
 
-$conn = new db_access();
+$db = new db_access();
+
+$message = new messageService($db);
 
 //if ($method == 'GET') {
-//    $conn->GET($_GET['source'], $_GET['target']);
+//    $message->GET($_GET['source'], $_GET['target']);
 //} else {
-//    $conn->POST($_GET['source'], $_GET['target'], $_GET['message']);
+//    $message->POST($_GET['source'], $_GET['target'], $_GET['message']);
 //}
 
-$conn->POST("tittyFucker", "dickFucker", "This is a message");
+//$message->POST("woman", "man", "This is a message");
+$message->GET("man", "woman");
 
-$conn->GET("tittyFucker", "dickFucker");
-
-?>
+//$conn->GET("blah", "blah");
